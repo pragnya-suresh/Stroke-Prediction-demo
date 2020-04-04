4475e91bfc07bf573f926f664dc0606fe68ca5c1
Pragnya Suresh <pragnyasuresh@gmail.com>
2020-04-04 21:36:25 +0530

    added the health status api

--- backend.py
+++ backend.py
@@ -2,29 +2,30 @@ import os
 from flask import Flask, flash, request, redirect, url_for, jsonify,json
 import requests
 from flask_cors import CORS, cross_origin
-import numpy as np
 from flask import Response
 from flask import jsonify
+from sklearn.neighbors import NearestNeighbors
 import csv
 import random
 import time
 import os
 import pandas as pd
+import numpy as np
 import pickle
 
 app = Flask(__name__)
 
 CORS(app, support_credentials=True)
 
-df = pd.read_csv("final.csv" ,error_bad_lines=False)
-model = pickle.loads("model.pickle")
-
-#Run this block only one and then comment
-cols = ["id","timestamp"]
-cols.append(df.cols.tolist())
-with open('record.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(cols)
+#df = pd.read_csv("final.csv" ,error_bad_lines=False)
+#model = pickle.loads("model.pickle")
+#
+##Run this block only once and then comment
+#cols = ["id","timestamp"]
+#cols.append(df.cols.tolist())
+#with open('record.csv', 'w', newline='') as file:
+#    writer = csv.writer(file)
+#    writer.writerow(cols)
 
 
 @app.route('/result', methods =['GET'])
@@ -46,30 +47,61 @@ def get_result():
 def get_status():
     if(request.method=='GET'):
         id = request.args.get("id")
+        param = request.args.get("param")
         print("id:",id)
+        ts =df_patient[df_patient['id']==id]['timestamp'].values
         #retrive all the records with this id from the csv
+        X = df.loc[:, df.columns != 'stroke']
+        X = X.loc[:, df.columns != param]
         df_record = pd.read_csv("record.csv")
         df_patient = df.loc[df['id'] == id]
+        df_patient = df_patient.loc[:, df_patient.columns != 'id']
+        df_patient = df_patient.loc[:, df_patient.columns != 'timestamp']
+        df_patient = df_patient.loc[:, df_patient.columns != param]
+        prin(len(x),len(df_patient))
+        attr = df.columns.tolist()
+        neigh = NearestNeighbors(n_neighbors=10)
+        d = {}
+        for index, r in df_patient.iterrows():
+            row=[]
+            inner_d={}
+            for i in attr:
+              row.append(r[i])
+            print(row)
+            neigh.fit(X)
+            neighbours = neigh.kneighbors([row])
+            #gives a tuple(dist,index)
+            ind = []
+            for i in range(10):
+                ind.append(neighbours[1][0][i])
+            df_nearest = df.iloc[ind]
+            inner_d["min"] = df_nearest[param].min()
+            inner_d["param"] = param
+            inner_d["max"] = df_nearest[param].max()
+            d[ts[index]] = inner_d
+            
         #for each row in resulting df,for each attr, find 10 rows with least deviation w.r.t. other attributes, find min and max- return attr_val, min,max
         #return as dictionary of dictionaries
         #outer dictionary key will be timestamp
         #d["ts1"] = {"attr1":x,"min":y,"max":z}
         #d["ts2"] = {"attr2":x,"min":y,"max":z}
-        return jsonify(result=result), 200
+        
+        return jsonify(result=d), 200
     else:
         return jsonify(), 405
 
 #input- id, timestamp, attributes
-@app.route('/store', methods =['GET'])
+@app.route('/store', methods =['POST'])
 @cross_origin(supports_credentials=True)
 def store():
-    if(request.method=='GET'):
+    if(request.method=='POST'):
         inp=[]
-        d = request.args
-        l = list(d.values)
+        d = list(request.forms.keys())[0]
+        print("d:",d)
+
         with open('record.csv', 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([l])
+            writer.writerow([d])
         
         return jsonify(result=result), 200
     else:
